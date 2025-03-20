a5464d61eb5b49a14992910c766a98c59666dd38
Vlad <vladswork95@gmail.com>
2025-03-20 17:03:54 +0000

    For correction

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,8 @@
-export const removeAllContacts = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeAllContacts = () => {
+  writeContacts([]);
+  console.log('Contact list is empty');
+};
 
 removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contactList = await readContacts();
+  if (contactList.length > 0) {
+    const lastContact = contactList.pop();
+    console.log('Removed contact:', lastContact);
+    await writeContacts();
+  } else {
+    console.log('No contacts to remove');
+  }
+};
 
 removeLastContact();
